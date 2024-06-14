1fc51245b33dc2d58724dca90c6a8959021e0487
mstfunsal98 <mstfunsal98@outlook.com>
2024-06-14 21:36:02 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', event => {
+  console.log('Service worker installing...');
+  // Precache işlemi burada yapılabilir
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activating...');
+  // Eski önbellekleri temizleme işlemi burada yapılabilir
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
